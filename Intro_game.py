14106f38b2143f6625639dd7cee4cdf506a5f489
mathis-serra <mathis.serra@laplateforme.io>
2024-01-12 11:31:26 +0100

    added video but don t work actually

--- Intro_game.py
+++ Intro_game.py
@@ -1,25 +1,47 @@
 import pygame
+import os
+import cv2
 from Screen import Screen
-from Pyvidplayer import Video 
-
-
 pygame.init()
 
+# class Screen:
+#     def __init__(self):
+#         self.size = (1280, 7200)
+    
+#     def get_size(self):
+#         return self.size
 
 screen = Screen()
-vid = Video("Data/Intro/Pokemon_intro.mp4")
-vid.set_size(screen.get_size())
 
-def intro():
-    vid.draw(screen.get_display(), (0, 0))
-    pygame.display.update()
+chemin_video = os.path.join('Data/Intro/Pokemon_intro.mp4')
+cap = cv2.VideoCapture(chemin_video)
+
+ret, frame = cap.read()
+if not ret:
+    # Return to the beginning
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+# Resize the image to the size of the Pygame window
+frame = cv2.resize(frame, (1280, 720))
+frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+# Convert the OpenCV image to a Pygame surface
+frame = pygame.surfarray.make_surface(frame)
+
+# Create a Pygame window
+window = pygame.display.set_mode(screen.get_size())
+pygame.display.set_caption("pokemon")
+
+# Blit the frame onto the window
+window.blit(frame, (0, 0))
+pygame.display.flip()
+
+# Wait for the user to close the window
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
-            vid.close()
-            quit()
+            running = False
 
-intro()
-
-
-
-
+cap.release()
+pygame.quit()
