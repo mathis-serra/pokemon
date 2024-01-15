5c0090fd5ccb5cc599227b99bf4c82a32277ee55
kevin-ngo1 <kevin.ngo@laplateforme.io>
2024-01-15 01:03:22 +0100

    New Interface, Add Pokemon png in Combat, New Dialogue for Interface, Remove Rectangle, Change all the class InterfaceFight

--- Dialogue_game.py
+++ Dialogue_game.py
@@ -1,16 +1,43 @@
 import pygame
+from class_combat import Combat
 
-class InterfaceFight:
-    def __init__(self,x,y,width,height,color):
-        self.rect=pygame.Rect(x,y,width,height)
-        self.color=color
+class InterfaceFight(Combat):
+    def __init__(self, fenetre, pokemon1_id, pokemon2_id):
+        Combat.__init__(self, pokemon1_id, pokemon2_id)
+        self.fenetre = fenetre
+        self.largeur_fenetre = 800
+        self.hauteur_fenetre = 600
+        font = "Font/Pokemon_font.ttf"
+        self.font = pygame.font.Font(font,30)
+        self.dialogue_text = f"Un {self.pokemon2['name']['french']} sauvage apparaît !"
+        self.pokemon1_id = pokemon1_id
+        self.pokemon2_id = pokemon2_id
 
-    def draw(self,surface):
-        pygame.draw.rect(surface,self.color,self.rect)
+    def display_message(self, message):
+        lines = self.wrap_text(message, self.largeur_fenetre - 20)
+        y_offset = self.hauteur_fenetre - 120
 
-    def display_rect(self,surface):
-        self.draw(surface)
+        for line in lines:
+            text = self.font.render(line, True, (255, 255, 255))
+            text_rect = text.get_rect(center=(self.largeur_fenetre // 2, y_offset))
+            self.fenetre.blit(text, text_rect)
+            y_offset += self.font.get_height()
 
-    @classmethod
-    def deroulement(cls):
-        return cls(0, 400, 800, 200, ("#a0e8b1"))
+    def wrap_text(self, text, max_width):
+        words = text.split(' ')
+        lines = []
+        current_line = words[0]
+
+        for word in words[1:]:
+            test_line = current_line + ' ' + word
+            if self.font.size(test_line)[0] <= max_width:
+                current_line = test_line
+            else:
+                lines.append(current_line)
+                current_line = word
+
+        lines.append(current_line)
+        return lines
+
+    def change_text(self):
+        self.dialogue_text = f"Je t'envoie {self.pokemon1['name']['french']} !"
---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame
+import time
+from Dialogue_game import InterfaceFight
+
+pygame.init()
+pygame.mixer.init()
+
+# Fenetre
+largeur_fenetre = 800
+hauteur_fenetre = 600
+fenetre = pygame.display.set_mode((largeur_fenetre, hauteur_fenetre))
+background = pygame.image.load("Data/Combat/Combat_Sprite/Background/Forest_Background.png")
+background = pygame.transform.scale(background, (largeur_fenetre, hauteur_fenetre - 200))
+text_img = pygame.image.load("Data/Combat/Combat_Sprite/CombatUi/Bottom_message_Box.png")
+text_img = pygame.transform.scale(text_img, (largeur_fenetre, hauteur_fenetre - 400))
+pygame.display.set_caption("Pokemon Fight")
+
+# Son
+pygame.mixer.music.load("song/WildFight.mp3")
+pygame.mixer.music.set_volume(1)
+pygame.mixer.music.play(1)
+
+# Variables
+grey = "#8c8c8c"
+black = "#000000"
+green = "#a0e8b1"
+white = "#ffffff"
+interface_fight = InterfaceFight(fenetre,pokemon1_id=10, pokemon2_id=222)
+
+# Running Game et Event
+start_time = time.time()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                interface_fight.change_text()
+
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+
+    if elapsed_time < 3.2:
+        if int(elapsed_time / 0.1) % 2 == 0:
+            fenetre.fill(grey)
+        else:
+            fenetre.fill(black)
+    else:
+        fenetre.blit(text_img, (0, 400))
+        fenetre.blit(background, (0, 0))
+        interface_fight.display_message(interface_fight.dialogue_text)
+        pokemon1_image = pygame.image.load(f"Data/Pokemon/Pokemon_Sprites/back/{interface_fight.pokemon1_id}.png")
+        pokemon1_image = pygame.transform.scale(pokemon1_image, (200, 200))
+        fenetre.blit(pokemon1_image, (100, 240))
+        pokemon2_image = pygame.image.load(f"Data/Pokemon/Pokemon_Sprites/front/{interface_fight.pokemon2_id}.png")
+        pokemon2_image = pygame.transform.scale(pokemon2_image, (200, 200)) 
+        
+        fenetre.blit(pokemon2_image, (480, 75))
+
+    pygame.display.flip()
+
+pygame.quit()
