468b364dc1a0bfc78c3c9cf6c9a184fe9c9de4fc
WIlly 19 <william.rauwens-oliver@laplateforme.iogit config --global user.name>
2024-01-16 16:31:33 +0100

    edit files

--- Data/MapsPokemon/untitled-removebg-preview (1).tsx
+++ Data/MapsPokemon/untitled-removebg-preview (1).tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="untitled-removebg-preview (1)" tilewidth="16" tileheight="16" tilecount="527" columns="31">
- <image source="../../../../../../Downloads/untitled-removebg-preview (1).png" width="500" height="282"/>
+ <image source="untitled-removebg-preview (1).png" width="500" height="282"/>
 </tileset>
