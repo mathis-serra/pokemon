25600bbb230e4dc4ee63edef8303a56ee2a3ac6d
mathis-serra <mathis.serra@laplateforme.io>
2024-01-17 17:33:55 +0100

    re order files

---
+++ intro_main/Colors.py
@@ -0,0 +1,23 @@
+class Colors:
+    def __init__(self):
+        # Colors #
+        self.BLACK = (0, 0, 0)
+        self.WHITE = (255, 255, 255)
+        self.RED = (255, 0, 0)
+        self.GREEN = (0, 255, 0)
+        self.BLUE = (0, 0, 255)
+        self.YELLOW = (255, 255, 0)
+        self.PURPLE = (255, 0, 255)
+        self.CYAN = (0, 255, 255)
+        self.GRAY = (128, 128, 128)
+        self.LIGHT_GRAY = (192, 192, 192)
+        self.DARK_GRAY = (64, 64, 64)
+        self.BROWN = (128, 64, 0)
+        self.ORANGE = (255, 128, 0)
+        self.PINK = (255, 128, 128)
+        self.LIGHT_BLUE = (128, 128, 255)
+        self.LIGHT_GREEN = (128, 255, 128)
+        self.LIGHT_YELLOW = (255, 255, 128)
+        self.LIGHT_RED = (255, 128, 128)
+        self.LIGHT_PURPLE = (255, 128, 255)
+        self.LIGHT_CYAN = (128, 255, 255)
