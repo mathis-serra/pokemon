77912a09e684dd692c78a39180b8a4d419045125
mathis-serra <mathis.serra@laplateforme.io>
2024-01-17 16:33:13 +0100

    .

--- screen.py
+++ screen.py
@@ -3,24 +3,19 @@ import pygame
 
 class Screen:
     def __init__(self):
-        self.display: pygame.display = pygame.display.set_mode((1280, 720))
+        self.display = pygame.display.set_mode((1280, 720))
         pygame.display.set_caption("Pokémon")
-        self.clock: pygame.time.Clock = pygame.time.Clock()
-        self.framerate: int = 144
-        self.deltatime: float = 0.0
+        self.clock = pygame.time.Clock()
+        self.framerate = 60
 
-    def update(self) -> None:
+    def update(self):
         pygame.display.flip()
         pygame.display.update()
         self.clock.tick(self.framerate)
         self.display.fill((0, 0, 0))
-        self.deltatime = self.clock.get_time()
 
-    def get_delta_time(self) -> float:
-        return self.deltatime
-
-    def get_size(self) -> tuple[int, int]:
+    def get_size(self):
         return self.display.get_size()
 
-    def get_display(self) -> pygame.display:
+    def get_display(self):
         return self.display
