bcd46e98640ee2da34e0243d35cb3438c864d4ac
mathis-serra <mathis.serra@laplateforme.io>
2024-01-17 16:32:09 +0100

    all buttons from Menu.py working correctly need some adjustement and starting pokedex.py

--- Menu.py
+++ Menu.py
@@ -8,6 +8,7 @@ from game import Game
 import duel
 import sys
 import main as main
+from Button import Button
 
 class Menu():
     
@@ -22,7 +23,7 @@ class Menu():
         font = pygame.font.Font("Data/Game/Font/pokemon-emerald.ttf", 36)
         self.SCREEN.display.blit(self.SPRITES.main_menu_pokemon, (0, 0))
         new_game_button = Button(160, 60, self.SPRITES.menu_bar_pokemon, 1) 
-        continue_button = Button(160, 150, self.SPRITES.menu_bar_blue_pokemon, 1) 
+        continue_button = Button(160, 150, self.SPRITES.menu_bar_pokemon, 1) 
         mysyery_gift_button = Button(160, 240, self.SPRITES.menu_bar_pokemon, 1) 
         settings_button = Button(160, 330, self.SPRITES.menu_bar_pokemon, 1)
         pokedex_button = Button(160, 420, self.SPRITES.menu_bar_pokemon, 1) 
@@ -50,28 +51,27 @@ class Menu():
         self.SCREEN.display.blit(text_surface3, text_rect3)
         self.SCREEN.display.blit(text_surface4, text_rect4)
         self.SCREEN.display.blit(text_surface5, text_rect5)
-        self.SCREEN.update()
+        # self.SCREEN.update()
+        
+        
             
 
     def run(self):
         self.SPRITES = Sprites()
         self.main_menu()
         
+        selected_button = "new_game_button"
         
-        
+        menu_buttons = {
+        "new_game_button": Button(160, 60, self.SPRITES.menu_bar_pokemon, 1),
+        "continue_button": Button(160, 150, self.SPRITES.menu_bar_pokemon, 1),
+        "mysyery_gift_button": Button(160, 240, self.SPRITES.menu_bar_pokemon, 1),
+        "settings_button": Button(160, 330, self.SPRITES.menu_bar_pokemon, 1),
+        "pokedex_button": Button(160, 420, self.SPRITES.menu_bar_pokemon, 1),
+        }
         
         while True:
             
-            new_game_button = Button(160, 60, self.SPRITES.menu_bar_pokemon, 1) 
-            continue_button = Button(160, 150, self.SPRITES.menu_bar_blue_pokemon, 1) 
-            mysyery_gift_button = Button(160, 240, self.SPRITES.menu_bar_pokemon, 1) 
-            settings_button = Button(160, 330, self.SPRITES.menu_bar_pokemon, 1)
-            pokedex_button = Button(160, 420, self.SPRITES.menu_bar_pokemon, 1) 
-            self.SCREEN.display.blit(self.SPRITES.menu_bar_pokemon, (160, 510))
-            
-            
-                
-                
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -79,41 +79,47 @@ class Menu():
                     sys.exit()
                 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if new_game_button.draw(self.SCREEN.display):
+                    if selected_button in menu_buttons and menu_buttons[selected_button].draw(self.SCREEN.display):
                         self.SCREEN.display.fill((0, 0, 0))
-                        pygame.display.update()
+                        pygame.display.flip()
+                    if selected_button == "new_game_button":
                         game = Game()
                         game.run()
-                        self.main_menu()
-                        
-                    if continue_button.draw(self.SCREEN.display):
+                                        
+                    if selected_button == "continue_button" :
                         self.SCREEN.display.fill((0, 0, 0))
                         pygame.display.update()
                         duel.start()
                         self.main_menu()
                         
-                    if mysyery_gift_button.draw(self.SCREEN.display):
+                    if selected_button == "mysyery_gift_button" :
                         self.SCREEN.display.fill((0, 0, 0))
                         pygame.display.update()
                         duel.start()
                         self.main_menu()
                         
-                    if settings_button.draw(self.SCREEN.display):
+                    if selected_button == "settings_button" :
                         self.SCREEN.display.fill((0, 0, 0))
                         pygame.display.update()
                         duel.start()
                         self.main_menu()
                         
-                    if pokedex_button.draw(self.SCREEN.display):
+                    if selected_button == "pokedex_button" :
                         self.SCREEN.display.fill((0, 0, 0))
                         pygame.display.update()
                         duel.start()
                         self.main_menu()
-                    
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        return True
-                    if event.key == pygame.K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
+           
+            pygame.display.update()      
+            
+            
+        
 
+            
+            
+            
+if __name__ == "__main__":
+    menu = Menu()
+    menu.run()
+    pygame.quit()
+    sys.exit()
